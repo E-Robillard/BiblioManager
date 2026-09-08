990474670bd07b22eb979c3e43f8e16b6b0e0dfa
l.ridao <ridao.lionel@gmail.com>
2026-09-08 12:03:27 +0200

    classe Livre créé

---
+++ src/main/java/models/Livre.java
@@ -0,0 +1,64 @@
+package models;
+
+public class Livre {
+    private int id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+
+    public Livre(int id, String title, String author, String isbn, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + title + " (" + author + ") - "
+                + (available ? "Disponible" : "Indisponible");
+    }
+}
