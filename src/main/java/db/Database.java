b92f9598fca5f0792d14a2efef69a1af9bc0e8e0
E-Robillard <e.robillard@stag.technofuturtic.education>
2026-09-08 11:36:05 +0200

    Init: Add db

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class Database {
     private static final String URL = "jdbc:sqlite:library.db";
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
